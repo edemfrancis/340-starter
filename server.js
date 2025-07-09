b46ee7be5bacbb9433cb6c20d77e2562354ce3f9
edemfrancis <efrancis.edem@gmail.com>
2025-07-09 05:45:28 +0100

    Restarting this prject for better understanding

--- server.js
+++ server.js
@@ -9,11 +9,23 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
+
 
 /* ***********************
- * Routes
+ * View and Templates
  *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+/* ***********************
+ * Routes
+  *************************/
 app.use(static)
+// Index Route, the home page 
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
